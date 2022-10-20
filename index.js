e07ab5647beaf9e8bf977b10ef7dcc4775a4ee30
unknown <shvydchenkoandriy@gmail.com>
2022-10-20 16:01:42 +0300

    changes

--- index.js
+++ index.js
@@ -3,28 +3,38 @@
     items: [],
   };
 
-  function setState(state) {
+  function setState(newState) {
     state = newState;
+    render(state);
   }
+
+  function render(state) {
+    const app = document.getElementById("app");
+    app.innerHTML = `
+    <input type='checkbox' class='checkbox'>
+    <h2 class='txt'>Txt</h2>
+    <button class='butt1' id='butt'>delete</button>
+    <p>${JSON.stringify(state.items.length)}</p>`;
+  }
+  setState(state);
 
   function addItem(item) {
     const next = { title: item.title, completed: true };
     state.items.push(next);
-    setState(state)
+    setState(state);
   }
 
   function deleteItem(num) {
     state.items.splice(num, 1);
-    setState(state)
+    setState(state);
   }
 
   function toggleCompleted(num) {
     state.items[num].completed = !state.items[num].completed;
-    setState(state)
+    setState(state);
   }
-  
+
   addItem({ title: "foo1" });
   addItem({ title: "foo2" });
-  addItem({ title: "foo3" });
-  console.log(state)
+  console.log(state);
 })();
