4d8093a58b0b69d2e0953c785be996a069bb7822
unknown <shvydchenkoandriy@gmail.com>
2022-10-21 16:56:28 +0300

    try to add lestener to delete butt

--- index.js
+++ index.js
@@ -3,25 +3,31 @@
     items: [],
   };
   const app = document.getElementById("app");
+
   function setState(newState) {
     state = newState;
     render(state);
   }
 
-    function render(state) {
-      app.innerHTML = ''
-      for (let elem of state.items) {
-        app.innerHTML += `<div>
+  function render(state) {
+    app.innerHTML = "";
+    for (let elem of state.items) {
+      const elemInd = state.items.indexOf(elem);
+      app.innerHTML += `<div data-index="${elemInd}">
         <input type='checkbox' class='checkbox'>
         <h2 class='txt'>${elem.title}</h2>
         <button class='butt1' id='butt'>delete</button>
         </div>`;
-        }
-      app.innerHTML += `<p>${state.items.length}</p>`;
+       
+      const delButt = app.querySelector(".butt1");
+      delButt.addEventListener("click", () => {
+        const delButIdx = app.querySelector("div").getAttribute("data-index");
+        app.remove()
+        deleteItem(delButIdx)
+      });
     }
-  
-
-  
+    app.innerHTML += `<p>${state.items.length}</p>`;
+  }
 
   function addItem(item) {
     const next = { title: item.title, completed: true };
@@ -40,7 +46,8 @@
   }
 
   addItem({ title: "foo1" });
-  addItem({ title: "foo2" });
+
+
+  console.log(app);
   
-  console.log(state);
 })();
