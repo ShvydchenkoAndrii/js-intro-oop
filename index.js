46280a7ccce89e843b49a94d4fd093efd0e05c7f
unknown <shvydchenkoandriy@gmail.com>
2022-10-29 15:48:16 +0300

    added delete func on button

--- index.js
+++ index.js
@@ -3,12 +3,17 @@
     items: [],
   };
   const app = document.getElementById("app");
+  const count = document.getElementById("count");
 
   function setState(newState) {
     state = newState;
+
     render(state);
   }
-
+  function deleteItem(num) {
+    state.items.splice(num, 1);
+    setState(state);
+  }
   function render(state) {
     app.innerHTML = "";
     for (let elem of state.items) {
@@ -16,27 +21,22 @@
       app.innerHTML += `<div data-index="${elemInd}">
         <input type='checkbox' class='checkbox'>
         <h2 class='txt'>${elem.title}</h2>
-        <button class='butt1' id='butt'>delete</button>
+        <button class='butt1' id='butt' onclick="deleteItem(${elemInd})">delete</button>
         </div>`;
-
-      const delButt = app.querySelector(".butt1");
-      delButt.addEventListener("click", () => {
-        const delButIdx = app.querySelector("div").getAttribute("data-index");
-        app.remove();
-        deleteItem(delButIdx);
-      });
     }
     app.innerHTML += `<p>${state.items.length}</p>`;
   }
-
+  console.log(app);
+  // const delButt = app.querySelectorAll("button");
+  // for (let but of delButt) {
+  //   but.addEventListener("click", () => {
+  //     const delButIdx = but.closest("div").getAttribute("data-index");
+  //     deleteItem(delButIdx);
+  //   });
+  // }
   function addItem(item) {
     const next = { title: item.title, completed: true };
     state.items.push(next);
-    setState(state);
-  }
-
-  function deleteItem(num) {
-    state.items.splice(num, 1);
     setState(state);
   }
 
@@ -46,6 +46,5 @@
   }
 
   addItem({ title: "foo1" });
-
-  console.log(app);
+  addItem({ title: "foo2" });
 })();
