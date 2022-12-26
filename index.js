147cb4d7f4c1d791540ff5e59519e249ee43afdb
unknown <shvydchenkoandriy@gmail.com>
2022-12-26 19:07:36 +0200

    added togle func

--- index.js
+++ index.js
@@ -3,46 +3,37 @@
     items: [],
   };
   const app = document.getElementById("app");
-  const count = document.getElementById("count");
 
   function setState(newState) {
     state = newState;
-
     render(state);
   }
-  function deleteItem(num) {
-    state.items.splice(num, 1);
-    setState(state);
-  }
-  function render(state) {
-    app.innerHTML = "";
-    for (let elem of state.items) {
-      const elemInd = state.items.indexOf(elem);
-      app.innerHTML += `<div data-index="${elemInd}">
-        <input type='checkbox' class='checkbox'>
-        <h2 class='txt'>${elem.title}</h2>
-        <button class='butt1' id='butt' onclick="deleteItem(${elemInd})">delete</button>
-        </div>`;
-    }
-    app.innerHTML += `<p>${state.items.length}</p>`;
-  }
-  console.log(app);
-  // const delButt = app.querySelectorAll("button");
-  // for (let but of delButt) {
-  //   but.addEventListener("click", () => {
-  //     const delButIdx = but.closest("div").getAttribute("data-index");
-  //     deleteItem(delButIdx);
-  //   });
-  // }
   function addItem(item) {
     const next = { title: item.title, completed: true };
     state.items.push(next);
     setState(state);
   }
-
-  function toggleCompleted(num) {
-    state.items[num].completed = !state.items[num].completed;
+  window.deleteItem = (num) => {
+    state.items.splice(num, 1);
     setState(state);
+  };
+  window.toggleCompleted = (num) => {
+    state.items[num].completed = !state.items[num].completed;
+    console.log(state);
+    setState(state);
+  };
+
+  function render(state) {
+    app.innerHTML = "";
+    for (let elem of state.items) {
+      const elemInd = state.items.indexOf(elem);
+      app.innerHTML += `<div data-index="${elemInd}">
+        <input onchange='toggleCompleted(${elemInd})' type='checkbox' class='checkbox'>
+        <h2 class='txt'>${elem.title}</h2>
+        <button onclick="deleteItem(${elemInd})" class='butt1' id='butt' ">delete</button>
+        </div>`;
+    }
+    app.innerHTML += `<p>${state.items.length}</p>`;
   }
 
   addItem({ title: "foo1" });
