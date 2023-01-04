4fc1fdc2e4fd340be66a8d3dfc1984fbf77d1bbf
unknown <shvydchenkoandriy@gmail.com>
2023-01-04 13:26:58 +0200

    finished filter + localstorage

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 (function () {
   let state = {
     items: [],
+    filter: "all",
   };
   const app = document.getElementById("app");
 
   function setState(newState) {
     state = newState;
     render(state);
+    localStorage.setItem("ToDo", JSON.stringify(state));
   }
   window.addItem = () => {
     const inputVal = document.getElementById("inputToDo").value;
@@ -21,47 +23,63 @@
   window.toggleCompleted = (num) => {
     state.items[num].completed = !state.items[num].completed;
     setState(state);
-    console.log(state.items[num]);
   };
 
   window.completedFilter = () => {
-    const copy = { ...state };
-    const res = copy.items.filter((el) => el.completed === true);
-    copy.items = res;
-    render(copy);
+    setState({ ...state, filter: "completed" });
   };
   window.inProgressFilter = () => {
-    const copy = { ...state };
-    const res = copy.items.filter((el) => el.completed === false);
-    copy.items = res;
-    render(copy);
+    setState({ ...state, filter: "inProgress" });
   };
   window.allFilter = () => {
-    render(state);
+    setState({ ...state, filter: "all" });
   };
   window.clearState = () => {
     state.items = [];
     setState(state);
+    localStorage.clear();
   };
+
+  window.addEventListener("load", () => {
+    const localState = JSON.parse(localStorage.getItem("ToDo"));
+    if (localState) {
+      setState(localState);
+    }
+  });
 
   function render(state) {
     app.innerHTML = "";
     app.innerHTML += `<input type='text' id='inputToDo'><button onclick="addItem()">Add</button>`;
-    for (let elem of state.items) {
+
+    const filterResult = state.items.filter((item) => {
+      if (state.filter === "all") {
+        return item;
+      }
+      if (state.filter === "completed") {
+        return item.completed === true;
+      }
+      if (state.filter === "inProgress") {
+        return item.completed === false;
+      }
+    });
+
+    for (let elem of filterResult) {
       const elemInd = state.items.indexOf(elem);
       app.innerHTML += `<div data-index="${elemInd}">
-        <input  onchange="toggleCompleted(${elemInd})" ${
+      <input  onchange="toggleCompleted(${elemInd})" ${
         elem.completed ? "checked" : null
       } type='checkbox'>
-        <h2 class='txt'>${elem.title}</h2>
-        <button onclick="deleteItem(${elemInd})" class='butt1' id='butt' ">delete</button>
-        </div>`;
+      <h2 class='txt'>${elem.title}</h2>
+      <button onclick="deleteItem(${elemInd})" class='butt1' id='butt' ">delete</button>
+      </div>`;
     }
     app.innerHTML += `<p>${state.items.length}</p>`;
     app.innerHTML += `<button onclick="allFilter()">All</button>
-    <button onclick="completedFilter()">Completed</button>
-    <button onclick="inProgressFilter()">In Progress</button>
+    <button onclick="completedFilter()" id="completed">Completed</button>
+    <button onclick="inProgressFilter()" id="inProgress">In Progress</button>
     <button onclick="clearState()">Clear</button>`;
+    
   }
+
   render(state);
 })();
